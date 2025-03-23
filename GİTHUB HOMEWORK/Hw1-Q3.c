273c670a3f0d2cc8b67ab6a86bea8dcc55a1438f
LutfiYigitYazar <lyigityazar@gmail.com>
2025-03-23 17:25:26 +0300

    Update Hw1-Q3.c

--- GİTHUB HOMEWORK/Hw1-Q3.c
+++ GİTHUB HOMEWORK/Hw1-Q3.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double popularity,size,homeValue;
+    printf("Enter the popularity of the neighborhood: ");
+    scanf("%lf", &popularity);
+
+    printf("Enter the size of the house: ");
+    scanf("%lf", &size);
+
+    homeValue = (pow(popularity, 3) + pow(size, 2)) * 10000;
+
+    printf("The value of the home is: %lf TL\n", homeValue);
+
+    return 0;
+}
+
 
