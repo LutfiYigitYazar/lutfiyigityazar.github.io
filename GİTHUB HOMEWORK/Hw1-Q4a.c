71dd0899d4a5fbd9cee3f04756adff074a863216
LutfiYigitYazar <lyigityazar@gmail.com>
2025-03-23 17:53:58 +0300

    Update Hw1-Q4a.c

--- GİTHUB HOMEWORK/Hw1-Q4a.c
+++ GİTHUB HOMEWORK/Hw1-Q4a.c
@@ -1,1 +1,18 @@
-
+#include <stdio.h>
+#include <math.h>
+int computeHomeValue(int popularity, int size);
+int main(void) {
+    double homeValue, size, popularity;
+    printf("Enter the popularity of the neighborhood:\n");
+    scanf("%lf", &popularity);
+    
+    printf("Enter the size of the house\n");
+    scanf("%lf", &size);
+    homeValue = computeHomeValue(popularity, size);
+    printf("The value of the home: %lf", homeValue);
+} 
+int computeHomeValue(int popularity, int size) {
+    int homeValue;
+    homeValue = 10000 * ((pow(size, 2) + (pow(popularity, 3))));
+    return(homeValue);
+}
